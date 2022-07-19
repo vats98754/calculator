2e973b41b200c380d5a38b4dc8167a7d702332c1
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-19 17:52:24 +0600

    Write core calculator functions (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,13 @@
+// Core calculation functions of add, subtract, multiply, and divide
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return (a) * (b);
+}
+function divide(a, b) {
+    return (a) / (b);
+}
