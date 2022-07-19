957eb1d0067f3596922483ebe49165583e21e4c5
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-19 18:59:40 +0600

    Add 'operate' function, performs operation on operands Operate() calls appropriate core calculation func on the input operands given the operator (first parameter of operate())

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ const divide = function(a, b) {
     return (a) / (b);
 }
 
+// Function to perform calculation depending on input operator and two operands
+const operate = function(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Invalid operator';
+    }
+}
+
 // Exporting core calculation functions for Jest testing
 module.exports = {
     add,
