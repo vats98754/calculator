34649f16774354484eeb2a9ad8e4e065c93cc1a7
Anvay <vats98754@gapps.uwcsea.edu.sg>
2022-07-20 05:41:57 +0600

    Calc logic and clear/delete working; calc display needs reformatting

--- script.js
+++ script.js
@@ -28,6 +28,62 @@ const operate = function(operator, a, b) {
     }
 }
 
+const operators = {
+    '+': '+',
+    '-': '-',
+    '×': '*',
+    '÷': '/'
+};
+
+const history = document.querySelector('.history');
+const currNum = document.querySelector('.current-num');
+let firstNum = 0;
+let secondNum = 1;
+let currOperator = '+';
+
+// Add EventListener to each button to read its value
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const btnValue = btn.textContent;
+        if (currNum.textContent.includes('.')) {
+            btn.disabled = true;
+        } else {
+            btn.disabled = false;
+        }
+        if (operators[btnValue] !== undefined) {
+            const operator = operators[btnValue];
+            if (currNum.textContent === '' && operator === '-'){
+                currNum.textContent += operator;
+                history.textContent += `${firstNum} ${operator} `;
+            } else if (currNum.textContent !== '') {
+                currOperator = operator;
+                firstNum = currNum.textContent;
+                currNum.textContent = '';
+                history.textContent += `${firstNum} ${operator} `;
+            }
+        } else if (parseInt(btnValue) == btnValue) {
+            currNum.textContent += btnValue;
+        } else if (btnValue === 'AC') {
+            currNum.textContent = '';
+            history.textContent = '';
+            firstNum = 0;
+            secondNum = 1;
+        } else if (btnValue === '=') {
+            firstNum = history.textContent.split(" ")[0];
+            secondNum = currNum.textContent;
+            currNum.textContent = '';
+            history.textContent += `${secondNum} ${currOperator} =`;
+            const result = operate(currOperator, firstNum, secondNum);
+            currNum.textContent = result;
+        } else if (btnValue === '.') {
+            currNum.textContent += btnValue;
+        } else if (btnValue === '⌫') {
+            currNum.textContent = currNum.textContent.slice(0, -1);
+        }
+    });
+});
+
 // // Exporting core calculation functions for Jest testing
 // module.exports = {
 //     add,
